6ada2aa54114731b08ad71435f9eec87a58d780f
James Halliday <mail@substack.net>
2011-05-30 13:48:00 -0700

    deepEquiv seems to work but deepEqual doesn't

--- browser/test.js
+++ browser/test.js
@@ -48,6 +48,53 @@ Test.prototype.equal = function (x, y, desc) {
     if (this.running) {
         this.emit('fail', 'equal() called after test ended');
     }
-    else if (x == y) this.emit('ok', x, y, desc);
-    else this.emit('fail', x, y, desc);
+    else if (x == y) this.emit('ok', 'equal', x, y, desc);
+    else this.emit('fail', 'equal', x, y, desc);
 };
+
+Test.prototype.deepEqual = function (x, y, desc) {
+    if (this.running) {
+        this.emit('fail', 'deepEqual() called after test ended');
+    }
+    else if (deepEquiv(x, y)) {
+        this.emit('ok', 'deepEqual', x, y, desc);
+    }
+    else {
+        this.emit('fail', 'deepEqual', x, y, desc);
+    }
+};
+
+function deepEquiv (x, y) {
+    if (typeof x !== typeof y) {
+        return false;
+    }
+    else if (x.__proto__ !== y.__proto__) {
+        return false;
+    }
+    else if (typeof x === 'object') {
+        if (x === null || y === null) {
+            return x === y;
+        }
+        else if (Object.prototype.toString.call(x) === '[object Arguments]') {
+            if (Object.prototype.toString.call(y) !== '[object Arguments]') {
+                return false;
+            }
+        }
+        else if (x instanceof Date && y instanceof Date) {
+            return x.getTime() === y.getTime();
+        }
+        
+        var kx = Object.keys(x);
+        var ky = Object.keys(y);
+        if (kx.length !== ky.length) return false;
+        for (var i = 0; i < kx.length; i++) {
+            var k = kx[i];
+            if (x[k] === y[k]) continue;
+            if (!deepEquiv(x[k], y[k])) return false;
+        }
+        return true;
+    }
+    else {
+        return x === y;
+    }
+}
