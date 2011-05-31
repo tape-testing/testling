15e2004133150b06b5594d968e154364cb0a2b44
James Halliday <mail@substack.net>
2011-05-30 19:28:11 -0700

    toggle failing icon color

--- browser/main.js
+++ browser/main.js
@@ -29,13 +29,26 @@ $(window).ready(function () {
                 }).appendTo($('#tests'));
                 
                 var t = new Test(name, box.find('.frames'));
+                var arrow = box.find('div.title img');
+                var tArrow = total.find('div.title img');
                 
                 t.on('ok', function () {
                     box.vars.ok ++;
+                    total.vars.ok ++;
                 });
                 
                 t.on('fail', function () {
+                    box.addClass('failed');
                     box.vars.fail ++;
+                    
+                    [ arrow, tArrow ].forEach(function (elem) {
+                        var m = elem.attr('src')
+                            .match(/(.+\/(?:up|down))\.png/);
+                        if (m) elem.attr('src', m[1] + '_fail.png');
+                    });
+                    
+                    if (!total.hasClass('failed')) total.addClass('failed');
+                    total.vars.fail ++;
                 });
                 
                 test[name](t);
