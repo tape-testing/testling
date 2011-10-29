a6de2c86d35d10986edc305c72cf5b8c8e175756
James Halliday <mail@substack.net>
2011-10-29 03:24:37 -0700

    now with jquery

--- example/form.js
+++ example/form.js
@@ -1,7 +1,7 @@
 var test = require('testling');
 
 test('login', function (t) {
-    t.createWindow('http://localhost:8081', function (win) {
+    t.createWindow('http://localhost:8081', function (win, $) {
         var pending = 2;
         function finish () {
             if (--pending === 0) t.end();
@@ -14,11 +14,8 @@ test('login', function (t) {
             finish();
         });
         
-        for (var i = 0; i < form.elements.length; i++) {
-            var elem = form.elements[i];
-            if (elem.name === 'user') elem.value = 'abc';
-            if (elem.name === 'pass') elem.value = 'def';
-        }
+        $('input[name=user]', form).val('abc');
+        $('input[name=pass]', form).val('def');
         
         t.submitForm(form, function (w) {
             t.equal(w.document.body.innerHTML, 'welcome!');
