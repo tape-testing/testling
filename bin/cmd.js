df888cac802be8719959eee48132c0b205de34c4
James Halliday <mail@substack.net>
2012-06-23 14:22:10 -0700

    injection proxy works

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,1 +1,26 @@
 #!/usr/bin/env node
+var argv = require('optimist').argv;
+
+//var profileDir = '/tmp/' + Math.random().toString(16).slice(2);
+var profileDir = '/tmp/02faf1b1';
+var mkdirp = require('mkdirp').sync(profileDir);
+
+var script = '<script src="http://localhost:9595/proxy.js"></script>';
+var insertProxy = require('../lib/proxy');
+
+var port = 54045;
+var proxy = insertProxy(script, [ 'http://localhost:9595' ]);
+proxy.listen(port);
+
+var http = require('http');
+var ecstatic = require('ecstatic')(__dirname + '/../static');
+var server = http.createServer(ecstatic);
+server.listen(9595);
+console.log([
+    'no_proxy=""',
+    'google-chrome',
+    '--incognito',
+    '--proxy-server=localhost:' + port,
+    '--user-data-dir=' + profileDir,
+    'http://localhost:9595'
+].join(' '));
