50971346b313c716cbdc491d5fadd60d615cfae6
James Halliday <mail@substack.net>
2012-06-25 05:41:30 -0700

    fix option for headless and kill

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,27 +1,22 @@
 #!/usr/bin/env node
-var argv = require('optimist').argv;
+var argv = require('optimist')
+    .option('headless', { default : true, type : 'boolean' })
+    .option('kill', { default : true, type : 'boolean' })
+    .default('proxy', 'localhost:54045')
+    .default('server', 'localhost:54046')
+    .argv
+;
+
 var bundle = require('../lib/bundle')(argv._);
 
 var insertProxy = require('../lib/proxy');
 var producer = require('../lib/producer');
 
-//var profileDir = '/tmp/' + Math.random().toString(16).slice(2);
-var profileDir = '/tmp/02faf1b1';
-(function () {
-    var mkdirp = require('mkdirp');
-    mkdirp.sync(profileDir);
-})();
-
-var port = {
-    proxy : 54045,
-    server : 54046
-};
-
 var script = '<script src="'
-    + 'http://localhost:' + port.server + '/proxy.js'
+    + 'http://' + argv.server + '/proxy.js'
     + '"></script>'
 ;
-var proxy = insertProxy(script, [ 'http://localhost:' + port.server ]);
+var proxy = insertProxy(script, [ 'http://' + argv.server ]);
 var server = (function () {
     var http = require('http');
     var ecstatic = require('ecstatic')(__dirname + '/../static');
@@ -35,35 +30,41 @@ var server = (function () {
 })();
 
 var launcher = require('../lib/launcher');
-var browser;
 (function () {
     var pending = 2;
-    proxy.listen(port.proxy, onready);
-    server.listen(port.server, onready);
+    var ports = {
+        proxy : parseInt(argv.proxy.split(':')[1], 10),
+        server : parseInt(argv.server.split(':')[1], 10),
+    };
+    proxy.listen(ports.proxy, onready);
+    server.listen(ports.server, onready);
     function onready () {
         if (--pending !== 0) return;
-        browser = launcher({
-            proxy : port.proxy,
-            server : port.server,
-            profileDir : profileDir,
-        });
+        var browser = launcher(argv);
+        withBrowser(browser);
     }
 })();
 
 var JSONStream = require('JSONStream');
 var shoe = require('shoe');
-var sock = shoe(function (stream) {
-    stream
-        .pipe(JSONStream.parse([ true ]))
-        .pipe(producer())
-        .on('end', function () {
-            server.close();
-            proxy.close();
-            browser.kill();
-            
-            process.exit();
-        })
-        .pipe(process.stdout, { end : false })
-    ;
-});
-sock.install(server, '/push');
+
+function withBrowser (browser) {
+    var sock = shoe(function (stream) {
+        stream
+            .pipe(JSONStream.parse([ true ]))
+            .pipe(producer())
+            .on('end', function () {
+                if (argv.kill) {
+                    process.exit();
+                }
+            })
+            .pipe(process.stdout, { end : false })
+        ;
+    });
+    sock.install(server, '/push');
+    
+    browser.on('exit', function () {
+        server.close();
+        proxy.close();
+    });
+}
