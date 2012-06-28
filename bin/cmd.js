2fc0f0eb94c9c5ddc8ac660e85df030f36c4f94d
James Halliday <mail@substack.net>
2012-06-28 00:57:17 -0700

    now integrated with browser-launcher

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
+
+var launcher = require('browser-launcher');
+var insertProxy = require('../lib/proxy');
+var producer = require('../lib/producer');
+
 var argv = require('optimist')
     .option('headless', { default : true, type : 'boolean' })
     .default('proxy', 'localhost:54045')
     .default('server', 'localhost:54046')
     .argv
 ;
-
 var bundle = require('../lib/bundle')(argv._);
-
-var insertProxy = require('../lib/proxy');
-var producer = require('../lib/producer');
-
 var script = '<script src="'
     + 'http://' + argv.server + '/proxy.js'
     + '"></script>'
@@ -28,26 +28,42 @@ var server = (function () {
     });
 })();
 
-var launcher = require('../lib/launcher');
 (function () {
     var pending = 2;
     var ports = {
         proxy : parseInt(argv.proxy.split(':')[1], 10),
         server : parseInt(argv.server.split(':')[1], 10),
     };
+    var uri = 'http://localhost:' + ports.server + '/?' + Math.random();
+    
     proxy.listen(ports.proxy, onready);
     server.listen(ports.server, onready);
+    
     function onready () {
         if (--pending !== 0) return;
-        var browser = launcher(argv);
-        withBrowser(browser);
+        
+        launcher(function (err, launch) {
+            if (err) return console.error(err);
+            
+            var opts = {
+                headless : argv.headless,
+                browser : argv.browser,
+                version : argv.version,
+                proxy : 'localhost:' + ports.proxy,
+            };
+            launch(uri, opts, function (err, ps) {
+                if (err) console.error(err)
+                else withBrowser(ps)
+            });
+        });
     }
+    
 })();
 
 var JSONStream = require('JSONStream');
 var shoe = require('shoe');
 
-function withBrowser (browser) {
+function withBrowser (ps) {
     var sock = shoe(function (stream) {
         stream
             .pipe(JSONStream.parse([ true ]))
@@ -62,7 +78,7 @@ function withBrowser (browser) {
     });
     sock.install(server, '/push');
     
-    browser.on('exit', function () {
+    ps.on('exit', function () {
         server.close();
         proxy.close();
     });
