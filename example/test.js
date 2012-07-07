79d44bcc20e76e9ed08d7bfb09b4d66c4221f76d
Raynos <raynos2@gmail.com>
2012-07-07 12:36:05 -0700

    fix nested tests

--- example/test.js
+++ example/test.js
@@ -3,5 +3,9 @@ var test = require('../');
 test('json parse', function (t) {
     t.same(JSON.parse('[1,2]'), [1,2]);
     t.log('beep boop');
+    t.test('nested test', function (t) {
+        t.ok(true, "nested ok")
+        t.end()
+    })
     t.end();
 });
