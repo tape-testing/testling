c85152c4cd407adde7f6adee34f691e1e5e24ce4
Jordan Harband <ljharb@gmail.com>
2020-01-18 21:57:48 -0800

    [Refactor] elses should be cuddled

--- bin/cmd.js
+++ bin/cmd.js
@@ -46,8 +46,7 @@ if ((process.stdin.isTTY || argv._.length) && argv._[0] !== '-') {
                 + 'Consult the quick start guide for how to create one:\n'
                 + 'https://ci.testling.com/guide/quick_start'
             );
-        }
-        else {
+        } else {
             console.error(err.message);
         }
         return;
@@ -102,15 +101,14 @@ if ((process.stdin.isTTY || argv._.length) && argv._[0] !== '-') {
                 ps.on('exit', function (code) {
                     if (code !== 0) {
                         console.error('FAILURE: non-zero exit code');
+                    } else {
+                        ready();
                     }
-                    else ready();
                 });
-            }
-            else if (expanded.script.length) {
+            } else if (expanded.script.length) {
                 bundle = '';
                 ready();
-            }
-            else {
+            } else {
                 console.error('No test files, no scripts, and no html parameter found after expanding the globs. At least one file or a custom html field is needed.');
                 process.exit(1);
             }
@@ -120,8 +118,7 @@ if ((process.stdin.isTTY || argv._.length) && argv._[0] !== '-') {
     if (pkg.testling.html) {
         ready();
     }
-}
-else {
+} else {
     process.stdin.pipe(concat(function (src) {
         bundle = src;
         htmlQueue.forEach(function (f) { getHTML(f) });
@@ -149,21 +146,19 @@ var server = http.createServer(function (req, res) {
                 };
                 if (results.ok) {
                     process.exit(0);
+                } else {
+                    process.exit(1);
                 }
-                else process.exit(1);
             }));
         }));
         req.on('end', res.end.bind(res));
-    }
-    else if (u.replace(/\/$/, '') === '/__testling') {
+    } else if (u.replace(/\/$/, '') === '/__testling') {
         res.setHeader('content-type', 'text/html');
         getHTML(function (html) { res.end(html) });
-    }
-    else if (u.split('/')[1] === '__testling') {
+    } else if (u.split('/')[1] === '__testling') {
         req.url = req.url.replace(/^\/__testling/, '');
         ecstatic(req, res);
-    }
-    else {
+    } else {
         ecstatic(req, res);
     }
 });
@@ -172,7 +167,9 @@ server.listen(0, ready);
 var customServer = pkg.testling.server && (function () {
     var cmd = pkg.testling.server;
     if (!Array.isArray(cmd)) cmd = parseCommand(cmd);
-    if (/\.js$/.test(cmd[0])) cmd.unshift(process.execPath);
+    if (/\.js$/.test(cmd[0])) {
+        cmd.unshift(process.execPath);
+    }
 
     var env = copy(process.env);
     env.PORT = Math.floor((Math.pow(2, 16) - 10000) * Math.random() + 10000);
@@ -186,14 +183,13 @@ var customServer = pkg.testling.server && (function () {
     });
 
     return { port: env.PORT };
-})();
+}());
 
 var bouncer = bouncy(function (req, res, bounce) {
     var u = req.url.split('?')[0];
     if (!customServer || u.split('/')[1] === '__testling') {
         bounce(server.address().port);
-    }
-    else {
+    } else {
         bounce(customServer.port, { headers: { connection: 'close' } });
     }
 });
@@ -206,8 +202,7 @@ if ((argv.x || argv.bcmd) && typeof (argv.x || argv.bcmd) === 'boolean') {
 
 if (argv.u || argv.cmd || argv.x || argv.bcmd) {
     ready();
-}
-else {
+} else {
     launcher(function (err, launch_) {
         if (err) return console.error(err);
         launch = launch_;
@@ -246,15 +241,12 @@ function ready () {
                 );
             }
         });
-    }
-    else {
-        var browser =
-            launch &&
-            launch.browsers &&
-            (/linux|bsd/i.test(process.platform) // xvfb-run
+    } else {
+        var browser = launch
+            && launch.browsers
+            && (/linux|bsd/i.test(process.platform) // xvfb-run
                 ? launch.browsers.local[0]
-                : launch.browsers.local.filter(function(b) { return b.headless;
-                })[0]
+                : launch.browsers.local.find(function (b) { return b.headless; })
             );
 
         if (!browser) {
@@ -272,7 +264,9 @@ function ready () {
         };
 
         launch(href, opts, function (err, ps) {
-            if (err) return console.error(err);
+            if (err) {
+                return console.error(err);
+            }
             browserProcess = ps;
         });
     }
@@ -282,29 +276,31 @@ function getHTML (cb) {
 
     if (pkg.testling.html) {
         fs.readFile(path.join(dir, pkg.testling.html), function (err, src) {
-            if (err) console.error('while loading testling.html: ' + err);
-            else {
+            if (err) {
+                console.error('while loading testling.html: ' + err);
+            } else {
                 cb('<script>' + prelude + '</script>' + src);
             }
         });
         return;
     }
 
-    if (bundle === undefined) return htmlQueue.push(cb);
+    if (bundle === undefined) {
+        return htmlQueue.push(cb);
+    }
 
     var before = '', after = '';
     if (/^mocha(-|$)/.test(pkg.testling.harness)) {
-        var mochaFile = path.relative(dir,
+        var mochaFile = path.relative(
+            dir,
             resolve('mocha/mocha.js', { basedir: dir })
         );
         var m = /^mocha-(\w+)/.exec(pkg.testling.harness);
         var ui = m && m[1] || 'bdd';
-        before =
-            '<script src="/__testling/' + mochaFile + '"></script>'
+        before = '<script src="/__testling/' + mochaFile + '"></script>'
             + '<script>mocha.setup(' + JSON.stringify({
                 ui: ui, reporter: 'tap'
-            }) + ')</script>'
-        ;
+            }) + ')</script>';
         after = '<script>mocha.run()</script>';
     }
 
