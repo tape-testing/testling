0b44595444cfb3432342d0a2b0987e112e5ff07c
Jordan Harband <ljharb@gmail.com>
2022-09-19 17:07:59 -0700

    [Fix] avoid websocket prelude when `--html`
    Fixes #122

--- bin/cmd.js
+++ bin/cmd.js
@@ -273,7 +273,6 @@ function ready () {
 }
 
 function getHTML (cb) {
-
     if (pkg.testling.html) {
         fs.readFile(path.join(dir, pkg.testling.html), function (err, src) {
             if (err) {
@@ -306,7 +305,7 @@ function getHTML (cb) {
 
     cb('<!doctype html><html><head><meta charset="utf-8"></head><body>'
         + '<pre id="__testling_output"></pre>'
-        + '<script>' + prelude + '</script>'
+        + (argv.html ? '' : '<script>' + prelude + '</script>')
         + before
         + scripts.map(function (s) {
             return '<script src="/__testling/'
